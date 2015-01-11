fe585c9f98c1eb5fa8a98c6be57ecf4771a1bd2c
Ethan Mick <ethanmski@gmail.com>
2015-01-11 11:47:50 -0500

    Updated coffee script loader to use same options as .js loader.

--- src/node-loaders/coffee-script.js
+++ src/node-loaders/coffee-script.js
@@ -2,32 +2,49 @@ var fs = require("fs"),
     path = require("path");
 
 module.exports = function(blanket){
-    var coffeeScript = require("coffee-script");
-    var oldLoaderCS = require.extensions['.coffee'];
+  var coffeeScript = require("coffee-script");
+  var oldLoaderCS = require.extensions['.coffee'];
 
-    require.extensions['.coffee'] = function(localModule, filename) {
-        
-        var pattern = blanket.options("filter");
-        filename = blanket.normalizeBackslashes(filename);
-        if (blanket.matchPattern(filename,pattern)){
+  require.extensions['.coffee'] = function(localModule, filename) {
+    
+    var pattern = blanket.options("filter"),
+	reporter_options = blanket.options("reporter_options"),
+	originalFilename = filename,
+	inputFilename = filename;
+    filename = blanket.normalizeBackslashes(filename);
 
-            var content = fs.readFileSync(filename, 'utf8');
-            content = coffeeScript.compile(content);
-            blanket.instrument({
-                inputFile: content,
-                inputFileName: filename
-            },function(instrumented){
-                var baseDirPath = blanket.normalizeBackslashes(path.dirname(filename))+'/.';
-                try{
-                    instrumented = instrumented.replace(/require\s*\(\s*("|')\./g,'require($1'+baseDirPath);
-                    localModule._compile(instrumented, filename);
-                }
-                catch(err){
-                    console.log("Error parsing instrumented code: "+err);
-                }
-            });
-        }else{
-            oldLoaderCS(localModule,filename);
+    var antipattern = _blanket.options("antifilter");
+    if (typeof(antipattern) !== "undefined" && blanket.matchPattern(filename.replace(/\.js$/,""), antipattern)) {
+      oldLoader(localModule,filename);
+      if (_blanket.options("debug")) {
+	console.log("BLANKET-File will never be instrumented:"+filename);
+      }
+    } else if (blanket.matchPattern(filename,pattern)){
+      if (_blanket.options("debug")) {console.log("BLANKET-Attempting instrument of:"+filename);}
+      var content = fs.readFileSync(filename, 'utf8');
+      if (reporter_options && reporter_options.shortnames){
+          inputFilename = filename.replace(path.dirname(filename),"");
+      }
+      if (reporter_options && reporter_options.basepath){
+        inputFilename = filename.replace(reporter_options.basepath + '/',"");
+      }
+      content = coffeeScript.compile(content);
+      blanket.instrument({
+        inputFile: content,
+        inputFileName: inputFilename
+      },function(instrumented){
+        var baseDirPath = blanket.normalizeBackslashes(path.dirname(filename))+'/.';
+        try{
+          instrumented = instrumented.replace(/require\s*\(\s*("|')\./g,'require($1'+baseDirPath);
+          localModule._compile(instrumented, originalFilename);
         }
-    };
+        catch(err){
+          console.log("Error parsing instrumented code: "+err);
+	  throw new Error("BLANKET-Error parsing instrumented code: "+err);
+        }
+      });
+    }else{
+      oldLoaderCS(localModule,originalFilename);
+    }
+  };
 };
