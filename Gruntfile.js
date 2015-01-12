a66d680373f6e78b5c1ecfc86d4687836eb745c7
Ethan Mick <ethanmski@gmail.com>
2015-01-12 08:36:19 -0500

    Removed the rest of the distribution stuff.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,8 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> */\n\n',
     cmds: testConfig.cmds,
     runners: testConfig.runners,
-    phantom: testConfig.phantom,
     reporters: testConfig.reporters,
     blanketTest: {
       normal:{
@@ -20,56 +18,6 @@ module.exports = function(grunt) {
       },
       coverage:{
         node: "<%= cmds.mocha %> --reporter <%= reporters.mocha.node %> <%= runners.node %>",
-      }
-    },
-    concat: {
-      esprima: {
-        options: {
-          banner: '(function(define){\n',
-          footer: '\n})(null);<%= "" %>'
-        },
-        src: ['node_modules/esprima/esprima.js'],
-        dest: '.tmp/esprima.js'
-      },
-      falafel: {
-        options: {
-          banner: '/*!\n' +
-            ' * falafel (c) James Halliday / MIT License\n' +
-            ' * https://github.com/substack/node-falafel\n' +
-            ' */\n\n' +
-            '(function(require,module){\n',
-          footer: '\nwindow.falafel = module.exports;})(function(){return {parse: esprima.parse};},{exports: {}});'
-        },
-        src: ['node_modules/falafel/index.js'],
-        dest: '.tmp/falafel.js'
-      },
-      mocha: {
-        options: {
-          banner: '<%= banner %>'
-        },
-        src: [
-          '<%= concat.esprima.dest %>',
-          '<%= concat.falafel.dest %>',
-          'src/blanket.js',
-          'src/blanket_browser.js',
-          "src/qunit/reporter.js",
-          "src/config.js",
-          "src/blanketRequire.js",
-          "src/adapters/mocha-blanket.js"
-        ],
-        dest: 'dist/mocha/blanket_mocha.js'
-      }
-    },
-    uglify: {
-      options: {
-        preserveComments: require('uglify-save-license'),
-        beautify: {
-          ascii_only: true
-        }
-      },
-      mocha: {
-        src: ['dist/mocha/blanket_mocha.js'],
-        dest: 'dist/mocha/blanket_mocha.min.js'
       }
     },
     jshint: {
@@ -93,13 +41,9 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'src/*.js',
-        'src/adapters/*.js',
         'tests/*.js',
         'test-node/*.js'
       ]
-    },
-    clean: {
-      tmpfiles: ['./.tmp']
     },
     watch: {
       files: '<%= jshint.all %>',
@@ -110,7 +54,7 @@ module.exports = function(grunt) {
   // Load local tasks.
   grunt.loadTasks('tasks');
 
-  grunt.registerTask('build',['jshint', 'concat', 'uglify', 'clean']);
+  grunt.registerTask('build',['jshint']);
   grunt.registerTask('default', ['build', 'blanketTest']);
   grunt.registerTask('blanket', ['build', 'blanketTest:normal']);
   grunt.registerTask('blanket-coverage', ['build', 'blanketTest:coverage']);
